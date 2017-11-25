636a6b169b9540311642ce04dea60a6aead95585
kiran Nallamothu <kiran.git5151@gmail.com>
2017-11-25 14:20:34 +0530

    this is the first commit to mmove the project to github

---
+++ myfirstproject/src/myfirstproject/helloworld.java
@@ -0,0 +1,11 @@
+package myfirstproject;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome to hello world");
+	}
+
+}
